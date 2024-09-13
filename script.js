3da57a4f248a5f48d76621f1932f691e87a6394e
cyber-ground <81938276+cyber-ground@users.noreply.github.com>
2024-09-13 17:20:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -11,17 +11,8 @@
 const startBtn = document.querySelector('.start-btn'); 
   const gameClearMessage = document.querySelector('.game-clear');
   const gameOverMessage = document.querySelector('.game-over');
-
-  function init() {
-    startBtn.classList.add('js_displayNone');
-    gameClearMessage.classList.add('js_displayNone');
-    gameOverMessage.classList.add('js_displayNone');
-    startBtn.style.opacity = 0.5;
-    gameClearMessage.style.opacity = 1;
-    gameOverMessage.style.opacity = 1;
-  } init();
-  
 const cards = document.querySelectorAll('.memory-card');
+
   cards.forEach(card => {
     card.addEventListener('click', flipCard);
   });
@@ -77,26 +68,22 @@ function unMatchedCards() {
 }
 
 function gameClear() {
-  if(matched === cards.length / 2) {  
+  if(matched === cards.length / 2) {
     setTimeout(() => {
-      gameClearMessage.classList.remove('.js_displayNone');
-      gameClearMessage.classList.add('js_displayBlock');
+      gameClearMessage.classList.add('js_visible');
       disableCards();
       setTimeout(() => {
-        startBtn.classList.remove('.js_displayNone'); 
-        startBtn.classList.add('js_displayBlock');   
+        startBtn.classList.add('js_visible');
       }, 3000);
     }, 500);
   }
 }
 
-function gameOver() {
-  gameOverMessage.classList.remove('.js_displayNone');                             
-  gameOverMessage.classList.add('js_displayBlock');                                
-  disableCards();                                                     
-  setTimeout(() => {                                                
-    startBtn.classList.remove('.js_displayNone');                                   
-    startBtn.classList.add('js_displayBlock');                                   
+function gameOver() {        
+  gameOverMessage.classList.add('js_visible');                  
+  disableCards();                        
+  setTimeout(() => {
+    startBtn.classList.add('js_visible');                            
   }, 1500);  
 }
 
